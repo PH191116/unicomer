951eea27a3f5716589917340d764c8c9489328da
Mike Padilla <PH191116@alumno.udb.edu.sv>
2023-05-04 09:53:44 -0600

    FilterProxy configuration

--- src/main/java/com/org/unicomer/controllers/CandidatoController.java
+++ src/main/java/com/org/unicomer/controllers/CandidatoController.java
@@ -21,8 +21,6 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class CandidatoController {
     private final ICandidatoService candidatoService;
-    @CrossOrigin("https://unicomer-production.up.railway.app")
-
     @GetMapping("/")
     public ResponseEntity<Object> obtenerCandidatos(@RequestHeader("country") String country){
          try{
@@ -37,7 +35,6 @@ public class CandidatoController {
             return UtilResponse.generarResponse(HttpStatus.INTERNAL_SERVER_ERROR, "No se obtuvo comunicación... Intente más tarde");
         }
     }
-    @CrossOrigin("https://unicomer-production.up.railway.app")
     @PostMapping("/")
     public ResponseEntity<Object> insertarCandidato(@Valid @RequestBody Candidato candidato, @RequestHeader("country") String country){
         try{
@@ -52,7 +49,6 @@ public class CandidatoController {
             return UtilResponse.generarResponse(HttpStatus.INTERNAL_SERVER_ERROR, "No se obtuvo comunicación... Intente más tarde");
         }
     }
-    @CrossOrigin("https://unicomer-production.up.railway.app")
     @PutMapping("/{id}")
     public ResponseEntity<Object> actualizarCandidato(@Valid @RequestBody Candidato candidato, @RequestHeader("country") String country,
                                                         @PathVariable("id") Integer id){
