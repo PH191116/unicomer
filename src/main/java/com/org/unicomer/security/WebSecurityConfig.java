3a25adeea086a20f0588d3886b008ea4b5877378
Mike Padilla <PH191116@alumno.udb.edu.sv>
2023-05-04 11:31:46 -0600

    FilterProxy

--- src/main/java/com/org/unicomer/security/WebSecurityConfig.java
+++ src/main/java/com/org/unicomer/security/WebSecurityConfig.java
@@ -3,18 +3,15 @@ package com.org.unicomer.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests()
-// Las vistas públicas no requieren autenticación
-                    .requestMatchers("/api/**").permitAll()
-// Todas las demás URLs de la Aplicación requieren autenticación
-                    .anyRequest().permitAll();
-// El formulario de Login no requiere autenticacion
+            http.httpBasic().disable().authorizeHttpRequests().requestMatchers("/api/**").permitAll();
             return http.build();
         }
 
